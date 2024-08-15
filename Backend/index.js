4b4b216559fef62d6a3041a64518c716e0ea9ade
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-08-15 10:31:09 +0530

    server is running on 3000 and db is also connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,20 @@
-import express from "express"
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connection from "./config/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("server is running fine")
+})
+
+app.listen(PORT, async() => {
+  try {
+    await connection
+    console.log(`server is running on ${PORT} and db is also connected`);
+  } catch (error) {
+    console.log("error in the server", error);
+  }
+})
