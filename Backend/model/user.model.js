d6e147d3aa9dc7ce329919ac2b5fa448a21df80c
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-08-19 18:21:03 +0530

    Model is created for user

---
+++ Backend/model/user.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  gender: { type: String, required: true },
+  age: { type: Number, required: true }
+})
+
+const UserModel = mongoose.model("User", userSchema);
+
+export default UserModel;
