d9905d917ac95d72616b4ff807bbcc18b4daa2aa
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-08-21 01:22:00 +0530

    Login and Register part done

--- Backend/route/user.route.js
+++ Backend/route/user.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import UserModel from "../model/user.model";
 
 const userRouter = express.Router();
@@ -17,6 +18,29 @@ userRouter.post("/register", async (req, res) => {
     });
     await user.save();
     res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.SECRET_KEY);
+    return res.status(200).json({
+      message: "User logged in successfully",
+      token,
+    });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
